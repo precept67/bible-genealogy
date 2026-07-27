c2c364354ae344607e172e7c1b93250b57df9837
SangHyun Kim <sanghyunkim@SangHyunui-Macmini.local>
2026-07-28 00:48:51 +0900

    enable descendant dragging and keyboard nudging by default (holding Alt/Shift key to bypass)

--- app_v16_v2.js
+++ app_v16_v2.js
@@ -1929,7 +1929,7 @@ function nudgeSelectedPerson(amount, isVertical = false, forceMoveDescendants = 
   if (selectedPersonIds.size === 0) return;
   pushHistoryState();
   
-  const moveDescendants = false; // Enforce moving only selected objects
+  const moveDescendants = !forceMoveDescendants; // Move descendants by default, hold Shift key to move only selected card
   
   let allTargets = new Set();
   selectedPersonIds.forEach(personId => {
@@ -1977,7 +1977,7 @@ function nudgePersonDirect(personId, amount, isVertical = false, adjustCamera = 
   
   selectedPersonId = personId;
   
-  const moveDescendants = false; // Enforce moving only selected objects
+  const moveDescendants = !forceMoveDescendants; // Move descendants by default, hold Shift key to move only selected card
   
   let targets = [];
   if (moveDescendants) {
@@ -4826,7 +4826,7 @@ function renderTree() {
           updateTransform();
         }
         
-        const moveDescendants = false; // Enforce moving only selected objects
+        const moveDescendants = !e.altKey; // Move descendants by default, hold Alt key to move only selected card
         
         descendantDragData = [];
         
